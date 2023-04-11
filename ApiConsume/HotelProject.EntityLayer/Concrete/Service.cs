b1e295568c69de068af77d56ffbd019cadb8a082
Selim Akay <selim.akay@bilgeadamboost.com>
2023-04-11 09:28:21 +0300

    Nuget paketleri yüklendi. Layerlar arası referans işlemi yapıldı.

--- ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HotelProject.EntityLayer.Concrete
 {
-    internal class Service
+    public class Service
     {
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } 
     }
 }
