cc7579926eb1c5c27093ad29ce7d58303f55698b
Selim Akay <selim.akay@bilgeadamboost.com>
2023-04-12 16:43:05 +0300

    Controller'ların bir kısmı yazıldı

--- ApiConsume/HotelProject.WebApiConsume/Program.cs
+++ ApiConsume/HotelProject.WebApiConsume/Program.cs
@@ -2,18 +2,35 @@ using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.DataAccessLayer.EntityFramework;
 using HotelProject.EntityLayer.Concrete;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<Context>();
+
 builder.Services.AddScoped<IStaffDal, EfStaffDal>();
 //builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+//builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+//builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+//builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+//builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelProject.WebApi", Version = "v1" });
+});
 
 var app = builder.Build();
 
