67bb31e6a65a733616af31a6cf127046fd4143ad
akayslm <73195655+akayslm@users.noreply.github.com>
2023-04-30 23:11:43 +0300

    Yeni personel eklenmesi kısmı eklendi

--- FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -22,6 +23,25 @@ namespace HotelProject.WebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync(); //gelen veri jsondata değişkenine atanır
                 var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData); //gelen json tipindeki veriyi deserialize ederek tabloda gösterilebiliecek şekle dönüştürdük
                 return View(values);    
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "applcation/json");
+            var responseMessage = await client.PostAsync("http://localhost:5271/api/Staff", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
