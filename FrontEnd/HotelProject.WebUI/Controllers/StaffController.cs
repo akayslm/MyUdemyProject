dc7954ec5ac048a34348dac5c1b5be2aec5db3a3
akayslm <73195655+akayslm@users.noreply.github.com>
2023-05-01 13:49:43 +0300

    Personelin güncellenmesi işlemi eklendi

--- FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -69,5 +69,19 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();  
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5271/api/Staff/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {                
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
