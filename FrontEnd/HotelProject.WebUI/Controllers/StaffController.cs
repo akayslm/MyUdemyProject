df967095d6ce7d8f8418f96ad6b3d1afed1120eb
akayslm <73195655+akayslm@users.noreply.github.com>
2023-05-01 12:16:09 +0300

    Personelin silinmesi kısmı eklendi

--- FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -45,5 +45,16 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+        
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5271/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
