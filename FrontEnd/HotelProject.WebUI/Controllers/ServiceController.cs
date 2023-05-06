fc1f09056df3d636931e27b138b4ea32c24688f6
akayslm <73195655+akayslm@users.noreply.github.com>
2023-05-06 17:33:24 +0300

    Hizmetlerin silinmesi kısmı eklendi

--- FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -50,5 +50,15 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5271/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
